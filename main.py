b64b5a96aa0914ab87ce209127bd43a1bb28d816
BahAilime <emilia.beguin@gmail.com>
2024-12-28 18:47:57 +0100

    New param: usrrname that change the title

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ def home():
 @app.route("/badge/")
 def get_badge():
     idd = request.args.get("id")
+    username = request.args.get("username")
     url = f"https://us-central1-cssbattleapp.cloudfunctions.net/getRank?userId={idd}"
     response = requests.get(url)
     data = response.json()
     data["id"] = response.json()
 
     data["score"] = round(data["score"], 2)
+    data["name"] = username if username else "CSSBattle"
     # data["meanScore"] = round(float(data["score"]) / float(data["playedCount"]), 2)
 
     return render_template("badge.html", data=data)
